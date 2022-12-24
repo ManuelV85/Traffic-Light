f8ce4fc6f47be5236e9c0b8cc7297e7937ce7898
ManuelV85 <manuel.villater@gmail.com>
2022-12-24 04:29:55 +0000

    falta terminar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [ color, setColor] = useState("red");
+	if(color=="red"){
+		return (
+			<div className="trafficLight">
+				<div className="red selected" onClick={()=> setColor("")}></div>
+				<div className="yellow" onClick={()=> setColor("yellow")}></div>
+				<div className="green" onClick={()=> setColor("green")}></div>
+				
+			</div>
+		);
+	} if(color=="yellow"){
+		return (
+			<div className="trafficLight">
+				<div className="red " onClick={()=> setColor("red")}></div>
+				<div className="yellow selected" onClick={()=> setColor("")}></div>
+				<div className="green" onClick={()=> setColor("green")}></div>
+				
+			</div>
+		);
+
+	} if(color=="green"){
+		return (
+			<div className="trafficLight">
+				<div className="red" onClick={()=> setColor("red")}></div>
+				<div className="yellow" onClick={()=> setColor("yellow")}></div>
+				<div className="green selected" onClick={()=> setColor("")}></div>
+				
+			</div>
+		);
+	} if(color!=""){
+		return(
+			
+			<h1>Invalid Traffic Light</h1>
+		)
+	}
+	//Invalid color we have it. Once no color selected then clicked on the red color we will get issue.
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="trafficLight">
+			<div className="red" onClick={()=> setColor("black")}></div>
+			<div className="yellow" onClick={()=> setColor("yellow")}></div>
+			<div className="green" onClick={()=> setColor("green")}></div>
+			
 		</div>
 	);
 };
